34c06beb2b7f653a31919f4fe591ec81e30da6c5
Alex Shallcross <ashallcross@ukcloud.com>
2021-08-04 14:45:00 +0100

    Added client interface profile and port selector

--- sandbox/modules/vmware-pod/main.tf
+++ sandbox/modules/vmware-pod/main.tf
@@ -2,6 +2,10 @@ resource "aci_leaf_profile" "vmware" {
   for_each = var.interface_map
 
   name = join("", [var.pod_id, "_vmware_", each.key])
+
+  relation_infra_rs_acc_port_p = [
+    aci_leaf_interface_profile.client_esx[each.key].id
+  ]
 }
 
 resource "aci_leaf_selector" "vmware" {
@@ -20,4 +24,19 @@ resource "aci_node_block" "vmware" {
   name  = join("", [var.pod_id, "_", each.key])
   from_ = each.value.node_ids[0]
   to_   = each.value.node_ids[1]
+}
+
+resource "aci_leaf_interface_profile" "client_esx" {
+  for_each = var.interface_map
+
+  name = join("", [var.pod_id, "_", each.key, "_client_esx"])
+}
+
+resource "aci_access_port_selector" "client_esx" {
+  for_each = var.interface_map
+
+  leaf_interface_profile_dn      = aci_leaf_interface_profile.client_esx[each.key].id
+  name                           = join("", [var.pod_id, "_", each.key, "_client_esx"])
+  access_port_selector_type      = "range"
+  #relation_infra_rs_acc_base_grp = aci_leaf_access_port_policy_group.client_esx.id
 }
