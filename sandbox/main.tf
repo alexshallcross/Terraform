c44731bd228892d5005e3514bc6a2124e9715598
Alex Shallcross <ashallcross@ukcloud.com>
2021-09-28 09:42:39 +0100

    Elevated UKCloud mgmt

--- sandbox/main.tf
+++ sandbox/main.tf
@@ -98,8 +98,6 @@ module "fabric_base" {
     903 = "10.41.3.82"
     904 = "10.41.3.90"
   }
-  assured_ukcloud_mgmt_ospf_interface_vlan = 3964
-
   assured_ukcloud_mgmt_ospf_interface_list = {
     node_901_17 = {
       node_id      = 901
@@ -142,8 +140,59 @@ module "fabric_base" {
       addr         = "10.41.0.94/30"
     }
   }
-
+  assured_ukcloud_mgmt_ospf_interface_vlan = 3964
   assured_ukcloud_mgmt_ospf_area_id = "0.0.0.5"
+
+  elevated_ukcloud_mgmt_rtr_ids = {
+    901 = "10.41.38.66"
+    902 = "10.41.38.74"
+    903 = "10.41.38.82"
+    904 = "10.41.38.90"
+  }
+  elevated_ukcloud_mgmt_ospf_interface_list = {
+    node_901_17 = {
+      node_id      = 901
+      interface_id = "eth1/33"
+      addr         = "10.41.36.66/30"
+    },
+    node_901_18 = {
+      node_id      = 901
+      interface_id = "eth1/34"
+      addr         = "10.41.36.70/30"
+    },
+    node_902_17 = {
+      node_id      = 902
+      interface_id = "eth1/33"
+      addr         = "10.41.36.78/30"
+    },
+    node_902_18 = {
+      node_id      = 902
+      interface_id = "eth1/34"
+      addr         = "10.41.36.74/30"
+    },
+    node_903_17 = {
+      node_id      = 903
+      interface_id = "eth1/33"
+      addr         = "10.41.36.82/30"
+    },
+    node_903_18 = {
+      node_id      = 903
+      interface_id = "eth1/34"
+      addr         = "10.41.36.86/30"
+    },
+    node_904_17 = {
+      node_id      = 904
+      interface_id = "eth1/33"
+      addr         = "10.41.36.90/30"
+    },
+    node_904_18 = {
+      node_id      = 904
+      interface_id = "eth1/34"
+      addr         = "10.41.36.94/30"
+    }
+  }
+  elevated_ukcloud_mgmt_ospf_interface_vlan = 3964
+  elevated_ukcloud_mgmt_ospf_area_id = "0.0.0.5"
 
 }
 
--- sandbox/modules/fabric-base/main.tf
+++ sandbox/modules/fabric-base/main.tf
@@ -854,8 +854,8 @@ resource "aci_vlan_pool" "assured_ukcloud_mgmt" {
 
 resource "aci_ranges" "assured_ukcloud_mgmt" {
   vlan_pool_dn = aci_vlan_pool.assured_ukcloud_mgmt.id
-  from         = "vlan-3964"
-  to           = "vlan-3964"
+  from         = "vlan-${var.assured_ukcloud_mgmt_ospf_interface_vlan}"
+  to           = "vlan-${var.assured_ukcloud_mgmt_ospf_interface_vlan}"
   alloc_mode   = "static"
 }
 
@@ -876,6 +876,12 @@ resource "aci_vrf" "assured_ukcloud_mgmt" {
 resource "aci_ospf_interface_policy" "assured_ukcloud_mgmt" {
   tenant_dn = aci_tenant.assured_ukcloud_mgmt.id
   name      = "ospf_int_p2p_protocol_policy_assured_ukcloud_mgmt"
+  ctrl = [
+    "advert-subnet",
+    "bfd",
+    "mtu-ignore"
+  ]
+  nw_t = "p2p"
 }
 
 resource "aci_l3_outside" "assured_ukcloud_mgmt" {
@@ -1023,4 +1029,117 @@ resource "aci_rest" "leaf_switch_profile_dom_policy_selector" {
   depends_on = [
     aci_rest.leaf_switch_profile_dom_leaf
   ]
+}
+
+#####################
+#### Assured CDS ####
+#####################
+
+resource "aci_tenant" "elevated_cds" {
+  name = "elevated_cds"
+}
+
+####################################
+#### Elevated UKCloud Management ####
+####################################
+
+resource "aci_vlan_pool" "elevated_ukcloud_mgmt" {
+  name       = "vlan_static_l3_out_elevated_ukcloud_mgmt"
+  alloc_mode = "static"
+}
+
+resource "aci_ranges" "elevated_ukcloud_mgmt" {
+  vlan_pool_dn = aci_vlan_pool.elevated_ukcloud_mgmt.id
+  from         = "vlan-3964"
+  to           = "vlan-3964"
+  alloc_mode   = "static"
+}
+
+resource "aci_l3_domain_profile" "elevated_ukcloud_mgmt" {
+  name                      = "l3_out_elevated_ukcloud_mgmt"
+  relation_infra_rs_vlan_ns = aci_vlan_pool.elevated_ukcloud_mgmt.id
+}
+
+resource "aci_tenant" "elevated_ukcloud_mgmt" {
+  name = "elevated_ukcloud_mgmt"
+}
+
+resource "aci_vrf" "elevated_ukcloud_mgmt" {
+  tenant_dn = aci_tenant.elevated_ukcloud_mgmt.id
+  name      = "elevated_ukcloud_mgmt"
+}
+
+resource "aci_ospf_interface_policy" "elevated_ukcloud_mgmt" {
+  tenant_dn = aci_tenant.elevated_ukcloud_mgmt.id
+  name      = "ospf_int_p2p_protocol_policy_elevated_ukcloud_mgmt"
+  ctrl = [
+    "advert-subnet",
+    "bfd",
+    "mtu-ignore"
+  ]
+  nw_t = "p2p"
+}
+
+resource "aci_l3_outside" "elevated_ukcloud_mgmt" {
+  tenant_dn = aci_tenant.elevated_ukcloud_mgmt.id
+  name      = "elevated_ukcloud_mgmt"
+
+  relation_l3ext_rs_ectx = aci_vrf.elevated_ukcloud_mgmt.id
+  relation_l3ext_rs_l3_dom_att = aci_l3_domain_profile.elevated_ukcloud_mgmt.id
+}
+
+resource "aci_logical_node_profile" "elevated_ukcloud_mgmt" {
+  l3_outside_dn = aci_l3_outside.elevated_ukcloud_mgmt.id
+  name          = "elevated_ukcloud_mgmt"
+}
+
+resource "aci_logical_node_to_fabric_node" "elevated_ukcloud_mgmt" {
+  logical_node_profile_dn = aci_logical_node_profile.elevated_ukcloud_mgmt.id
+  for_each                = var.elevated_ukcloud_mgmt_rtr_ids
+  tdn                     = "topology/pod-1/node-${each.key}"
+  rtr_id                  = each.value
+}
+
+resource "aci_logical_interface_profile" "elevated_ukcloud_mgmt" {
+  logical_node_profile_dn = aci_logical_node_profile.elevated_ukcloud_mgmt.id
+  name                    = "ospf_int_profile_elevated_ukcloud_mgmt"
+}
+
+resource "aci_l3out_ospf_interface_profile" "elevated_ukcloud_mgmt" {
+  logical_interface_profile_dn = aci_logical_interface_profile.elevated_ukcloud_mgmt.id
+  auth_key                     = "key"
+  auth_key_id                  = "1"
+  auth_type                    = "md5"
+  relation_ospf_rs_if_pol      = aci_ospf_interface_policy.elevated_ukcloud_mgmt.id
+}
+
+resource "aci_l3out_path_attachment" "elevated_ukcloud_mgmt" {
+  for_each = var.elevated_ukcloud_mgmt_ospf_interface_list
+
+  logical_interface_profile_dn = aci_logical_interface_profile.elevated_ukcloud_mgmt.id
+  target_dn                    = "topology/pod-1/paths-${each.value.node_id}/pathep-[${each.value.interface_id}]"
+  if_inst_t                    = "sub-interface"
+  addr                         = each.value.addr
+  encap                        = "vlan-${var.elevated_ukcloud_mgmt_ospf_interface_vlan}"
+  encap_scope                  = "local"
+  mode                         = "regular"
+  mtu                          = "9000"
+}
+
+resource "aci_external_network_instance_profile" "elevated_ukcloud_mgmt" {
+  l3_outside_dn = aci_l3_outside.elevated_ukcloud_mgmt.id
+  name          = "all"
+}
+
+resource "aci_l3_ext_subnet" "elevated_ukcloud_mgmt_all" {
+  external_network_instance_profile_dn = aci_external_network_instance_profile.elevated_ukcloud_mgmt.id
+  ip                                   = "0.0.0.0/0"
+}
+
+resource "aci_l3out_ospf_external_policy" "elevated_ukcloud_mgmt" {
+  l3_outside_dn = aci_l3_outside.elevated_ukcloud_mgmt.id
+  area_cost     = "1"
+  area_ctrl     = "redistribute,summary"
+  area_id       = "${var.elevated_ukcloud_mgmt_ospf_area_id}"
+  area_type     = "regular"
 }
