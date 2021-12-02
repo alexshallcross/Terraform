250de343792d18b5c900d48c9474319e1e915e87
Alex <ashallcross@ukcloud.com>
2021-12-02 10:45:58 +0000

    bd_pod00008_client_cluster1_vmware

--- sys00003/main.tf
+++ sys00003/main.tf
@@ -183,7 +183,7 @@ resource "aci_application_epg" "pod00008_client_cluster1_vmotion" {
 resource "aci_application_epg" "pod00008_client_cluster1_vmware" {
   application_profile_dn = aci_application_profile.pod00008_client_cluster1.id
   name                   = "pod00008_client_cluster1_vmware"
-  relation_fv_rs_bd      = "uni/tn-skyscape_mgmt/BD-bd_pod00008_client_cluster1_vmware"
+  relation_fv_rs_bd      = aci_bridge_domain.bd_pod00008_client_cluster1_vmware.id
   relation_fv_rs_prov = [
     "uni/tn-common/brc-default",
   ]
@@ -681,6 +681,13 @@ resource "aci_bridge_domain" "bd_pod00008_client_cluster1_scaleio_mgmt" {
 resource "aci_bridge_domain" "bd_pod00008_client_cluster1_vmotion" {
   tenant_dn           = "uni/tn-skyscape_mgmt"
   name                = "bd_pod00008_client_cluster1_vmotion"
+  arp_flood           = "yes"
+  ep_move_detect_mode = "garp"
+}
+
+resource "aci_bridge_domain" "bd_pod00008_client_cluster1_vmware" {
+  tenant_dn           = "uni/tn-skyscape_mgmt"
+  name                = "bd_pod00008_client_cluster1_vmware"
   arp_flood           = "yes"
   ep_move_detect_mode = "garp"
 }
