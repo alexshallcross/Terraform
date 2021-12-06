2184634aa42eb6a96a8eb211ec64240dad743808
Alex <ashallcross@ukcloud.com>
2021-12-06 16:53:38 +0000

    BD-bd_pod0002b_openstack_mgmt_provisioning

--- sys00003/main.tf
+++ sys00003/main.tf
@@ -667,6 +667,13 @@ resource "aci_bridge_domain" "bd_pod0002b_openstack_mgmt" {
   ep_move_detect_mode = "garp"
 }
 
+resource "aci_bridge_domain" "bd_pod0002b_openstack_mgmt_provisioning" {
+  tenant_dn           = aci_tenant.skyscape_mgmt.id
+  name                = "bd_pod0002b_openstack_mgmt_provisioning"
+  arp_flood           = "yes"
+  ep_move_detect_mode = "garp"
+}
+
 ##########################################
 #### assured_protection tenant config ####
 ##########################################
