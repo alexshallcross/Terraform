50eef9bc87191b9c176e5f6607d5e984d86556d9
Tom Hallsmith <thallsmith@ukcloud.com>
2021-12-15 09:40:18 +0000

    feat: Updated variables file

--- modules/vmware_pod00035_lab/variables.tf
+++ modules/vmware_pod00035_lab/variables.tf
@@ -18,11 +18,11 @@ variable "link_level_policy" {
   description = <<EOT
   The DN of the Link Level Policy, e.g;
   uni/infra/hintfpol-40G
-  
-  You can also use the output value from the output value from the fabric_base module; 
+
+  You can also use the output value from the output value from the fabric_base module;
   module.fabric_base.aci_fabric_if_pol_40G
 
-  If this variable is not specified it will use the default value; 
+  If this variable is not specified it will use the default value;
   uni/infra/hintfpol-40G
   EOT
 }
@@ -31,13 +31,13 @@ variable "cdp_enabled_policy" {
   type        = string
   default     = "uni/infra/cdpIfP-cdp_enabled"
   description = <<EOT
-  The DN of the CDP Enabled Polic, e.g; 
+  The DN of the CDP Enabled Polic, e.g;
   uni/infra/cdpIfP-cdp_enabled
-  
-  You can also use the output value from the output value from the fabric_base module; 
+
+  You can also use the output value from the output value from the fabric_base module;
   module.fabric_base.aci_cdp_interface_policy_enabled
 
-  If this variable is not specified it will use the default value; 
+  If this variable is not specified it will use the default value;
   uni/infra/cdpIfP-cdp_enabled
   EOT
 }
@@ -46,13 +46,13 @@ variable "cdp_disabled_policy" {
   type        = string
   default     = "uni/infra/cdpIfP-cdp_disabled"
   description = <<EOT
-  The DN of the CDP Disabled Policy, e.g; 
+  The DN of the CDP Disabled Policy, e.g;
   uni/infra/cdpIfP-cdp_disabled
-  
-  You can also use the output value from the output value from the fabric_base module; 
+
+  You can also use the output value from the output value from the fabric_base module;
   module.fabric_base.aci_cdp_interface_policy_disabled
 
-  If this variable is not specified it will use the default value; 
+  If this variable is not specified it will use the default value;
   uni/infra/cdpIfP-cdp_disabled
   EOT
 }
@@ -61,13 +61,13 @@ variable "lldp_enabled_policy" {
   type        = string
   default     = "uni/infra/lldpIfP-lldp_enabled"
   description = <<EOT
-  The DN of the LLDP Enabled Policy, e.g; 
+  The DN of the LLDP Enabled Policy, e.g;
   uni/infra/lldpIfP-lldp_enabled
-  
-  You can also use the output value from the output value from the fabric_base module; 
+
+  You can also use the output value from the output value from the fabric_base module;
   module.fabric_base.aci_lldp_interface_policy_enabled
 
-  If this variable is not specified it will use the default value; 
+  If this variable is not specified it will use the default value;
   uni/infra/lldpIfP-lldp_enabled
   EOT
 }
@@ -76,13 +76,13 @@ variable "lldp_disabled_policy" {
   type        = string
   default     = "uni/infra/lldpIfP-lldp_disabled"
   description = <<EOT
-  The DN of the LLDP Disabled Policy, e.g; 
+  The DN of the LLDP Disabled Policy, e.g;
   uni/infra/lldpIfP-lldp_disabled
-  
-  You can also use the output value from the output value from the fabric_base module; 
+
+  You can also use the output value from the output value from the fabric_base module;
   module.fabric_base.aci_lldp_interface_policy_disabled
-  
-  If this variable is not specified it will use the default value; 
+
+  If this variable is not specified it will use the default value;
   uni/infra/lldpIfP-lldp_disabled
   EOT
 }
@@ -90,10 +90,10 @@ variable "lldp_disabled_policy" {
 variable "ukcloud_mgmt_tenant" {
   type        = string
   description = <<EOT
-  The DN of the assured_ukcloud_mgmt or elevated_ukcloud_mgmt tenant, e.g; 
+  The DN of the assured_ukcloud_mgmt or elevated_ukcloud_mgmt tenant, e.g;
   uni/tn-assured_ukcloud_mgmt
-  
-  You can also use the output value from the output value from the assured_ukcloud_mgmt or elevated_ukcloud_mgmt module; 
+
+  You can also use the output value from the output value from the assured_ukcloud_mgmt or elevated_ukcloud_mgmt module;
   module.assured_ukcloud_mgmt.tenant
   module.elevated_ukcloud_mgmt.tenant
   EOT
@@ -102,10 +102,10 @@ variable "ukcloud_mgmt_tenant" {
 variable "ukcloud_mgmt_l3_out" {
   type        = string
   description = <<EOT
-  The DN of the assured_ukcloud_mgmt or elevated_ukcloud_mgmt l3_out, e.g; 
+  The DN of the assured_ukcloud_mgmt or elevated_ukcloud_mgmt l3_out, e.g;
   uni/tn-assured_ukcloud_mgmt/out-assured_ukcloud_mgmt
-  
-  You can also use the output value from the output value from the assured_ukcloud_mgmt or elevated_ukcloud_mgmt module; 
+
+  You can also use the output value from the output value from the assured_ukcloud_mgmt or elevated_ukcloud_mgmt module;
   module.assured_ukcloud_mgmt.l3out
   module.elevated_ukcloud_mgmt.l3out
   EOT
@@ -114,10 +114,10 @@ variable "ukcloud_mgmt_l3_out" {
 variable "ukcloud_mgmt_vrf" {
   type        = string
   description = <<EOT
-  The DN of the assured_ukcloud_mgmt or elevated_ukcloud_mgmt vrf, e.g; 
+  The DN of the assured_ukcloud_mgmt or elevated_ukcloud_mgmt vrf, e.g;
   uni/tn-assured_ukcloud_mgmt/ctx-assured_ukcloud_mgmt
-  
-  You can also use the output value from the output value from the assured_ukcloud_mgmt or elevated_ukcloud_mgmt module; 
+
+  You can also use the output value from the output value from the assured_ukcloud_mgmt or elevated_ukcloud_mgmt module;
   module.assured_ukcloud_mgmt.vrf
   module.elevated_ukcloud_mgmt.vrf
   EOT
@@ -126,10 +126,10 @@ variable "ukcloud_mgmt_vrf" {
 variable "protection_tenant" {
   type        = string
   description = <<EOT
-  The DN of the assured_protection or elevated_protection tenant, e.g; 
+  The DN of the assured_protection or elevated_protection tenant, e.g;
   uni/tn-assured_protection
-  
-  You can also use the output value from the output value from the assured_protection or elevated_ukcloud_mgmt module; 
+
+  You can also use the output value from the output value from the assured_protection or elevated_ukcloud_mgmt module;
   module.assured_protection.tenant
   module.elevated_protection.tenant
   EOT
@@ -138,10 +138,10 @@ variable "protection_tenant" {
 variable "protection_l3_out" {
   type        = string
   description = <<EOT
-  The DN of the assured_protection or elevated_protection l3_out, e.g; 
+  The DN of the assured_protection or elevated_protection l3_out, e.g;
   uni/tn-assured_protection/out-assured_protection
-  
-  You can also use the output value from the output value from the assured_protection or elevated_protection module; 
+
+  You can also use the output value from the output value from the assured_protection or elevated_protection module;
   module.assured_protection.l3out
   module.elevated_protection.l3out
   EOT
@@ -150,10 +150,10 @@ variable "protection_l3_out" {
 variable "protection_vrf" {
   type        = string
   description = <<EOT
-  The DN of the assured_protection or elevated_protectionvrf, e.g; 
+  The DN of the assured_protection or elevated_protectionvrf, e.g;
   uni/tn-assured_protection/ctx-assured_protection
-  
-  You can also use the output value from the output value from the assured_protection or elevated_protection module; 
+
+  You can also use the output value from the output value from the assured_protection or elevated_protection module;
   module.assured_protection.vrf
   module.elevated_protection.vrf
   EOT
@@ -164,19 +164,14 @@ variable "cimc_subnets" {
   description = "List of subnets in the podxxxxx_cimc bridge domain"
 }
 
-variable "storage_mgmt_subnets" {
-  type        = list(string)
-  description = "List of subnets in the podxxxxx_storage_mgmt bridge domain"
-}
-
 variable "mgmt_cluster_vmware_subnets" {
   type        = list(string)
   description = "List of subnets in the podxxxxx_mgmt_cluster_vmware bridge domain"
 }
 
-variable "mgmt_cluster_tools_subnets" {
+variable "mgmt_cluster_vsan_subnets" {
   type        = list(string)
-  description = "List of subnets in the podxxxxx_mgmt_cluster_tools bridge domain"
+  description = "List of subnets in the podxxxxx_mgmt_cluster_vsan bridge domain"
 }
 
 variable "mgmt_cluster_vmotion_subnets" {
@@ -184,34 +179,24 @@ variable "mgmt_cluster_vmotion_subnets" {
   description = "List of subnets in the podxxxxx_mgmt_cluster_vmotion bridge domain"
 }
 
-variable "mgmt_cluster_avamar_subnets" {
+variable "mgmt_cluster_transit_subnets" {
   type        = list(string)
-  description = "List of subnets in the podxxxxx_mgmt_cluster_avamar bridge domain"
+  description = "List of subnets in the podxxxxx_mgmt_cluster_transit bridge domain"
 }
 
-variable "client_cluster_1_vmware_subnets" {
+variable "mgmt_cluster_host_overlay_subnets" {
   type        = list(string)
-  description = "List of subnets in the podxxxxx_client_cluster_1_vmware bridge domain"
+  description = "List of subnets in the podxxxxx_mgmt_cluster_host_overlay bridge domain"
 }
 
-variable "client_cluster_1_vmotion_subnets" {
+variable "mgmt_cluster_edge_overlay_subnets" {
   type        = list(string)
-  description = "List of subnets in the podxxxxx_client_cluster_1_vmotion bridge domain"
-}
-
-variable "client_cluster_1_vxlan_subnets" {
-  type        = list(string)
-  description = "List of subnets in the podxxxxx_client_cluster_1_vxlan bridge domain"
+  description = "List of subnets in the podxxxxx_mgmt_cluster_edge_overlay bridge domain"
 }
 
 variable "mgmt_vmm_subnets" {
   type        = list(string)
   description = "List of subnets in the podxxxxx_mgmt_vmm bridge domain"
-}
-
-variable "client_avamar_subnets" {
-  type        = list(string)
-  description = "List of subnets in the podxxxxx_client_avamar bridge domain"
 }
 
 variable "vmm_ci" {
